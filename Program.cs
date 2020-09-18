a4eaca161aa0232668156df99763646f94768acd
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-18 15:45:47 -0300

    Iniciando

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORDER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
