5024905c62483b242ac3de9c952af905c05a9c4e
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-18 16:17:02 -0300

    Inserindo dados na classe OrderItem

--- Entities/OrderItem.cs
+++ Entities/OrderItem.cs
@@ -6,5 +6,24 @@ namespace ORDER.Entities
 {
     class OrderItem
     {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
     }
 }
