9d34cbf23b5766c022e772a7b71801d9187eb8d6
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-18 16:57:59 -0300

    Inserindo os dados na classe Order

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -2,9 +2,41 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ORDER.Entities
+namespace ORDER.Entities.Enums
 {
     class Order
     {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem Item)
+        {
+            Items.Add(Item);        }
+        public void RemoveItem(OrderItem Item)
+        {
+            Items.Remove(Item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum = sum + item.SubTotal();
+            }
+            return sum;
+        }
     }
 }
